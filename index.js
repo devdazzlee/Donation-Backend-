980f0cb6f2d87fb32012d8acfd7452adf8f76e59
Aisha <aisha.developer.20@gmail.com>
2024-05-28 14:46:59 +0500

    Changes

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 8000
-app.use(cors({
-    origin: [   'http://localhost:3000'   , "*"],
-    credentials: true
-}));
+app.use(cors())
+app.options('*', cors());
+
 
 app.use(bodyParser.json());
 
